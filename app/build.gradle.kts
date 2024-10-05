51dd4fccbbe24025dfa072e2922874f69775c646
moreoronce <nsy.bati@gmail.com>
2024-10-05 12:47:44 +0800

    delete unnecessary import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.okhttp)
-    implementation(libs.okhttp.logging)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
